7cb864fe3362daa5cec5eb8e2c4b8094b81e88f8
franclobo <fjbl2788@gmail.com>
2022-05-25 20:32:28 -0500

    Add structure and functions

---
+++ src/index.js
@@ -0,0 +1,22 @@
+import { nameInput, scoreInput, container, scoresList, Score } from './modules/setup.js'
+const addBtn = document.getElementById('submit');
+const data = document.getElementById('data');
+
+const printList = () => {
+  for (let i = 0; i < scoresList.length; i += 1) {
+    const scoreStorage = document.createElement('li');
+    scoreStorage.textContent = `${scoresList[i].name}: ${scoresList[i].score}`;
+    scoreStorage.className = 'scoreStorage';
+    container.append(scoreStorage);
+  }
+};
+
+printList();
+
+addBtn.addEventListener('click', (e) => {
+  e.preventDefault();
+  const score = new Score(nameInput.value, scoreInput.value);
+  score.addNew(score);
+  data.reset();
+  window.location.reload();
+});
